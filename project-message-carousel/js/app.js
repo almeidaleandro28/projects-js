b5d469776fa4d8265686b9e49b17c8f401f646ce
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2022-07-16 10:46:11 -0300

    new archives

---
+++ project-message-carousel/js/app.js
@@ -0,0 +1,7 @@
+
+
+
+const avatar = document.querySelector(".card-avatar");
+const name = document.querySelector(".card-name");
+const job = document.querySelector(".card-job");
+const information = document.querySelector(".card-info");
--- project-message-carousel/js/reviews.js
+++ project-message-carousel/js/reviews.js
@@ -1,7 +1,9 @@
-const reviews = {
-  id: 1,
-  name: "Karen R. Troutman",
-  job: "UX",
-  avatar: "people-ux.jpg",
-  comment: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, dicta. Amet aut voluptatem rerum sint dolorem omnis nisi temporibus ratione quaerat quis natus provident ducimus blanditiis magni inventore, veritatis quae?"
-} 
+[
+{
+  "id": 1,
+  "name": "Karen R. Troutman",
+  "job": "UX",
+  "avatar": "people-ux.jpg",
+  "comment": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, dicta. Amet aut voluptatem rerum sint dolorem omnis nisi temporibus ratione quaerat quis natus provident ducimus blanditiis magni inventore, veritatis quae?"
+ }
+] 
